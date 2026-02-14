ee5a2479e1baf397c3abc78ddeb10ad897dc8f02
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2026-02-14 12:12:36 -0300

    refactor: priva os setters das proprieades e trata os valores recebidos ao instânciar um usuário novo

--- App/Features/Users/Models/User.cs
+++ App/Features/Users/Models/User.cs
@@ -4,18 +4,18 @@ namespace App.Features.Users.Models;
 
 public sealed class User : Entity
 {
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public string Phone { get; set; }
-    public DateTime Created { get; set; }
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+    public string Phone { get; private set; }
+    public DateTime Created { get; private set; }
 
     public User(string name, string email, string password, string phone)
     {
         Name = name;
-        Email = email;
+        Email = email.ToLower();
         Password = password;
-        Phone = phone;
+        Phone = phone.Replace("-", "").Replace("(", "").Replace(")", "");
         Created = DateTime.UtcNow;
     }
 
