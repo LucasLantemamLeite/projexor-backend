68022b8418075261719569f01eb4b357e3d76736
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2026-02-14 11:03:07 -0300

    feat: executa os extension method de configuração ao iniciar a API

--- App/Program.cs
+++ App/Program.cs
@@ -1,5 +1,11 @@
+using App.Extensions.Config;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.ApplyBuildConfig(builder.Configuration);
+
 var app = builder.Build();
+
+app.ApplyAppConfig();
 
 app.Run();
